64ca3aae16e0d474e44c3b8e7b7dfb33497d6e37
ColinWan <colinwanuoft@gmail.com>
2021-04-07 12:35:20 -0400

    baseline

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ train_transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(hue=.1, saturation=.1, contrast=.1),
     transforms.RandomRotation(20, resample=Image.BILINEAR),
-    transforms.GaussianBlur(7, sigma=(0.1, 1.0)),
+#     transforms.GaussianBlur(7, sigma=(0.1, 1.0)),
     transforms.ToTensor(),  # convert PIL to Pytorch Tensor
     normalize,
 ])
